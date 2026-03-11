5fe6a2ea9a9149d723ee59526245f73dda5119c6
qkrckstjq <cksk49@naver.com>
2026-03-11 16:15:09 +0900

    [feat] add boundweapon interface

--- Source/BoundWeapon/IBoundWeaponRuntime.cs
+++ Source/BoundWeapon/IBoundWeaponRuntime.cs
@@ -1,13 +1,13 @@
 using Verse;
 using Verse.AI;
 
-//인터페이스 추상화
 namespace BoundWeapon
 {
     public interface IBoundWeaponRuntime
     {
-        bool IsEquipped(Pawn pawn, ThingWithComps weapon);
-        bool TryEquipFromInventory(Pawn pawn, ThingWithComps weapon);
-        Job TryCreateEquipJob(Pawn pawn, ThingWithComps weapon);
+        bool CanAssignToSlot(Pawn pawn, ThingWithComps weapon, BoundWeaponSlot slot);
+        bool IsEquipped(Pawn pawn, ThingWithComps weapon, BoundWeaponSlot slot);
+        bool TryEquipFromInventory(Pawn pawn, ThingWithComps weapon, BoundWeaponSlot slot);
+        Job TryCreateEquipJob(Pawn pawn, ThingWithComps weapon, BoundWeaponSlot slot);
     }
 }
